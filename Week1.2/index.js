5850384c4aeb60066245f3d11bc9f8b8fc02aa1c
Yash <wyash2752@gmail.com>
2023-12-05 15:36:32 +0530

    First week class files

---
+++ Week1.2/index.js
@@ -0,0 +1,44 @@
+// let a = [
+//   {
+//     name: "Yash",
+//     gender: "Male",
+//   },
+//   {
+//     name: "Rhitik",
+//     gender: "Male",
+//   },
+//   {
+//     name: "Priya",
+//     gender: "Female",
+//   },
+// ];
+
+// for (let i = 0; i < a.length; i = i + 1) {
+//   if (a[i]["gender"] == "Female") {
+//     console.log(a[i]["name"]);
+//   }
+// }
+
+// let sum = 0;
+// for (let i = 0; i < 100000000000; i++) {
+//   sum = sum + i;
+// }
+
+// console.log(sum);
+
+// function sum(num1, num2, functiontoCall) {
+//   let result = num1 + num2;
+//   functiontoCall(result);
+// }
+
+// function displayResult(data) {
+//   console.log("Result of the sum is : " + data);
+// }
+
+// function displayResultPassive(data) {
+//   console.log("Sum's result is : " + data);
+// }
+
+// let sum2 = sum(2, 3, displayResult);
+// // You are only allowed to call one function after this
+// // How will you displayResult of a sum
